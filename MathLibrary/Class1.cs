7075f773f74aa321d8208acadafb735efbd368df
jsnurmi <jsnurminen@gmail.com>
2015-04-19 23:47:17 +0300

    Changes to readme.md

--- MathLibrary/Class1.cs
+++ MathLibrary/Class1.cs
@@ -40,19 +40,15 @@ namespace MathLibrary
         {
             try
             {
-                //Grab your values and build your Web Request to the API
+             
                 string apiURL = String.Format("https://www.google.com/finance/converter?a={0}&from={1}&to={2}&meta={3}", amount, fromCurrency, toCurrency, Guid.NewGuid().ToString());
 
-                //Make your Web Request and grab the results
                 var request = WebRequest.Create(apiURL);
 
-                //Get the Response
                 var streamReader = new StreamReader(request.GetResponse().GetResponseStream(), System.Text.Encoding.ASCII);
 
-                //Grab your converted value (ie 2.45 USD)
                 var result = Regex.Matches(streamReader.ReadToEnd(), "<span class=\"?bld\"?>([^<]+)</span>")[0].Groups[1].Value;
 
-                //Get the Result
                 return result;
             }
             catch (Exception ex)
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # binaarilaskukone
 Ohjelmistotuotteen ylläpito -kurssin harjoitustyö
+
+Tomi Saari
+OHJ11 
+
+Jukka Nurminen
+OHJ11
+1102318
+
+Ohjelman toimintoja:
+
+1. Binäärilukumuunnin
+	- Syötä haluamasi binääriluku niin ohjelma muuntaa sen
+
+2. Valuuttamuunnin
+	- Syötä ensin jokin rahasumma. Tämän jälkeen syötä mistä valuutasta muunnetaan 
+	  esim. USD ja sen jälkeen mihin valuuttaan esim. EUR
+
+3. Luonnollisten lukujen summa
+	- Lasketaan lukujen 3 ja 5 jaollisten lukujen summa
+
+4. Potenssien erotus
+	- Lasketaan sadan ensimmäisen luonnollisen luvun toisen potenssien summa ja 
+	  sadan esnsimmäisen luvun summan potenssi, jotka vähennetään toisistaan.
+
+5. 10001:n alkuluku
+	- Lasketaan 10001:n alkuluku
+
+6. Viimeisin vastaus rekisteristä
+	- Palautetaan viimeisin vastaus rekisteristä. Kun jokin edellä mainituista toiminnoista
+	  suoritetaan, niin sen vastaus tallennetaan rekisteriin.
